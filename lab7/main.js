fb7cc84da912187d913d4482911000d661dfd6e0
joanaclaudio <a22100503@alunos.ulht.pt>
2024-10-31 11:55:12 +0000

    lab7

---
+++ lab7/main.js
@@ -0,0 +1,31 @@
+
+
+function mudarTexto (){
+    const frase  = document.querySelector('p');
+    frase.innerHTML = '1. Obrigado por passares!';
+}
+function reporTexto(){
+    const frase  = document.querySelector('p');
+    frase.innerHTML = '1. Passa por aqui!';
+}
+
+function mudarCor(cor){
+
+    const texto = document.querySelector('#texto');
+    texto.style.color = cor;
+    
+}
+const cores = [`#add8e6`, `#ffcccb`, `#fffacd`, `#d3d3d3`];
+let indiceCor = 0;
+function mudarFundoCaixa(){
+    
+    const input = document.querySelector('#inputTexto'); // Seleciona o elemento pelo ID
+    input.style.backgroundColor = cores[indiceCor]; // Muda a cor de fundo
+    indiceCor = (indiceCor + 1) % cores.length;
+}
+document.querySelector('#inputTexto').addEventListener('input', mudarFundoCaixa);
+
+function mudarCorFundoPagina(){
+    const cor = document.querySelector('#inputTexto').value;
+    document.body.style.backgroundColor = cor; 
+}
