9c99a53c45c03f7c19b9151b6fa162571748b697
joanaclaudio <a22100503@alunos.ulht.pt>
2024-11-14 11:58:36 +0000

    Update lab9

--- lab9/main.js
+++ lab9/main.js
@@ -1,32 +1,34 @@
 
 
-import {produtos} from './produtos'
+// Selecionar os elementos da página
+function carregarProdutos(){
+        
+    const produtosContainer = document.getElementById('produtos');
+    const cestoContainer = document.getElementById('cesto');
+
+    // Função para renderizar os produtos
+
+    produtos.forEach(produto => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <h2>${produto.title}</h2>
+        <img src = "${produto.image}" alt="${produto.title}"></img> 
+        <p>Custo total: ${produto.price}</p>
+        <p>${produto.description}</p>
+
+        <button onclick="adicionarAoCesto(${produto.id})">+ Adicionar ao Cesto</button>
+        `
+        produtosContainer.append(article);
+
+    });
+}
+
 
 document.addEventListener('DOMContentLoaded', () => { 
-
-    // Selecionar os elementos da página
-    function carregarProdutos(produtos){
-        
-        const produtosContainer = document.getElementById('produtos');
-        const cestoContainer = document.getElementById('cesto');
-
-        // Função para renderizar os produtos
-
-        produtos.forEach(produto => {
-            const article = document.createElement('article');
-            article.innerHTML = `
-            <h2>${produto.title}</h2>
-            <img src = "${produto.image}" alt="${produto.title}"></img> 
-            <p>Custo total: ${produto.price}</p>
-            <p>${produto.description}</p>
-
-            <button onclick="adicionarAoCesto(${produto.id})">+ Adicionar ao Cesto</button>
-            `
-            produtosContainer.append(article);
-
-        });
-    }
+    carregarProdutos();
+    
 });
+
 
 
 
