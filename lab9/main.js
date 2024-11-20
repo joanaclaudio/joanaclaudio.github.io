3f3b567167576b06825a82aedf47b3ccbc139275
joanaclaudio <a22100503@alunos.ulht.pt>
2024-11-20 11:40:40 +0000

    Lab9

--- lab9/main.js
+++ lab9/main.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => { 
+    carregarProdutos();
+    
+});
 
 
 // Selecionar os elementos da página
@@ -6,28 +10,49 @@ function carregarProdutos(){
     const produtosContainer = document.getElementById('produtos');
     const cestoContainer = document.getElementById('cesto');
 
-    // Função para renderizar os produtos
 
     produtos.forEach(produto => {
         const article = document.createElement('article');
-        article.innerHTML = `
-        <h2>${produto.title}</h2>
-        <img src = "${produto.image}" alt="${produto.title}"></img> 
-        <p>Custo total: ${produto.price}</p>
-        <p>${produto.description}</p>
+        const h2 = document.createElement('h2');
+        const img = document.createElement('img');
+        const p1 = document.createElement('p')
+        const p2 = document.createElement('p')
+        const button = document.createElement('button')
+        
+        h2.textContent = `${produto.title}`;
 
-        <button onclick="adicionarAoCesto(${produto.id})">+ Adicionar ao Cesto</button>
-        `
+        article.appendChild(h2);
+        
+        img.src = `${produto.image} `;
+        img.alt = ` ${produto.title}`;
+        img.height = 100;
+        img.width = 100;
+        article.appendChild(img);
+
+        p1.textContent = `Custo total: ${produto.price}`;
+
+        article.appendChild(p1)
+
+        p2.textContent = `${produto.description}`
+        p2.classList.add('box')
+        article.appendChild(p2)
+
+        button.onclick = () => {
+            adicionarAoCesto(produto.id);
+        };
+        button.textContent = `+ Adicionar ao Cesto`
+
+        article.appendChild(button)
+
+        article.classList.add('box2')
+
+        
         produtosContainer.append(article);
 
     });
 }
 
 
-document.addEventListener('DOMContentLoaded', () => { 
-    carregarProdutos();
-    
-});
 
 
 
